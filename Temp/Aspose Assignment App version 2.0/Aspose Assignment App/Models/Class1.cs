a4d5ca5b608da5775ad4bb575fd66782c8a4083b
Halimasadia65 <halimasadia65@gmail.com>
2016-08-19 15:57:38 +0500

    Revert "Revert "Aspose App version 2.0""
    This reverts commit a83bd93a7669eac83cda09053450180bf583875b.

---
+++ Temp/Aspose Assignment App version 2.0/Aspose Assignment App/Models/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aspose_Assignment_App.Models
+{
+     public class Class1
+     {
+          [Display(Name = "Select an Excel file")]
+          public string EFile { get; set; }
+
+          public string FullName { get; set; }
+          public string Email { get; set; }
+          public string Address { get; set; }
+          public string Salary { get; set; }
+          public List<Class1> Info { get; set; }
+     }
+}
---
+++ Temp/Aspose Assignment App version 2.0/Aspose Assignment App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aspose_Assignment_App.Startup))]
+namespace Aspose_Assignment_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
